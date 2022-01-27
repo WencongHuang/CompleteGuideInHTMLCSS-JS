52dcb4f893a494a7d61f25ede87aef3a60ac9267
Edmond Huang <vincent20100517@hotmail.com>
2022-01-26 19:52:11 -0600

    More JS

--- GlassUI/app.js
+++ GlassUI/app.js
@@ -1,2 +1,17 @@
+// Three ways to output text in JavaScript
+
+// Usually for debug?
 console.log("Hello World!");
+
+// This one create a pop up
+// same, but more practical
+// window.alert("Hello World!");
 alert("Hello World!");
+
+// This one actually write text on the website
+// document.write("Hello <em>World</em>!");
+
+// HTML Tag can also be use in JavaScript
+// <br> in HTML equal to document.write("<br>") in JavaScript;
+// Or we can also apply CSS using JavaScript.
+// document.write("<h1 style='color:red;'>Text</h1>");
