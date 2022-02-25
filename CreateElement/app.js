14a382ded66479ea8f3ef72b1b154ace492e8c4b
Edmond Huang <43325364+WencongHuang@users.noreply.github.com>
2022-02-25 15:43:14 -0600

    Finished Create Element
    Learned how to create element and add data into the table.

--- CreateElement/app.js
+++ CreateElement/app.js
@@ -1,3 +1,4 @@
+//Create Element and add data in list.
 let myTextBox = document.querySelector("#myTextBox");
 let myBtn = document.querySelector("#myBtn");
 let myList = document.querySelector("#myList");
@@ -10,4 +11,33 @@ myBtn.addEventListener('click', function() {
     myList.appendChild(myElement);
   }
   
+});
+
+
+//Create Element and add data in table.
+let nameTableBody = document.querySelector("#nameTableBody");
+let submitBtn = document.querySelector("#submitBtn");
+let firstName = document.querySelector("#firstName");
+let lastName = document.querySelector("#lastName");
+
+submitBtn.addEventListener("click", function() {
+  if(firstName.value.trim() == "" || lastName.value.trim() == "") {
+    alert("Please enter your first name and last name!");
+  }else{
+    let newRow = document.createElement("tr");
+
+    let newFirstName = document.createElement("td");
+    let newLastName = document.createElement("td");
+    newFirstName.innerHTML = firstName.value;
+    newLastName.innerHTML = lastName.value;
+    //put the data in the row
+    newRow.appendChild(newFirstName);
+    newRow.appendChild(newLastName);
+    //put the row in the table
+    nameTableBody.appendChild(newRow);
+
+    //reset the value so the user previous data gets clear.
+    firstName.value = "";
+    lastName.value = "";
+  }
 });
