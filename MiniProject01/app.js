4ddce2b80b3311fb67f23953f8894b019551b155
Edmond Huang <vincent20100517@hotmail.com>
2022-03-01 21:20:22 -0600

    Finished Mini Project 01
    Instead of adding a lot of h3 element, just create one h3 element and change/update its value. Also, uses Math.round() for dividing two numbers.

--- MiniProject01/app.js
+++ MiniProject01/app.js
@@ -7,36 +7,44 @@ let multiplyBtn = document.querySelector("#multiplyBtn");
 let divideBtn = document.querySelector("#divideBtn");
 let resetBtn = document.querySelector("#resetBtn");
 
-let displayBox = document.querySelector(".displayBox");
+// let displayBox = document.querySelector(".displayBox");
+let displayResult = document.querySelector("#displayResult");
 
 plusBtn.addEventListener("click", function() {
-  let result = document.createElement("h3");
-  result.innerHTML = Number(num1.value) + Number(num2.value);
-  displayBox.appendChild(result);
-  // num1.value = 0;
-  // num2.value = 0;
+  // let result = document.createElement("h3");
+  // result.innerHTML = Number(num1.value) + Number(num2.value);
+  // displayBox.appendChild(result);
+  displayResult.innerHTML = Number(num1.value) + Number(num2.value);
 });
 
 minusBtn.addEventListener("click", function() {
-  let result = document.createElement("h3");
-  result.innerHTML = Number(num1.value) - Number(num2.value);
-  displayBox.appendChild(result);
+  // let result = document.createElement("h3");
+  // result.innerHTML = Number(num1.value) - Number(num2.value);
+  // displayBox.appendChild(result);
+  displayResult.innerHTML = Number(num1.value) - Number(num2.value);
 });
 
 multiplyBtn.addEventListener("click", function() {
-  let result = document.createElement("h3");
-  result.innerHTML = Number(num1.value) * Number(num2.value);
-  displayBox.appendChild(result);
+  // let result = document.createElement("h3");
+  // result.innerHTML = Number(num1.value) * Number(num2.value);
+  // displayBox.appendChild(result);
+  displayResult.innerHTML = Number(num1.value) * Number(num2.value);
 });
 
 divideBtn.addEventListener("click", function() {
-  let result = document.createElement("h3");
+  // let result = document.createElement("h3");
+  // if(Number(num2.value) === 0) {
+  //   result.innerHTML = "undefined";
+  // }else{
+  //   result.innerHTML = Number(num1.value) / Number(num2.value);
+  // }
+  // displayBox.appendChild(result);
   if(Number(num2.value) === 0) {
-    result.innerHTML = "undefined";
+    displayResult.innerHTML = "undefined";
   }else{
-    result.innerHTML = Number(num1.value) / Number(num2.value);
+    // round the number to the 5 decimal point
+    displayResult.innerHTML = Math.round(((Number(num1.value) / Number(num2.value)) + Number.EPSILON) * 100000) / 100000;
   }
-  displayBox.appendChild(result);
 });
 
 resetBtn.addEventListener("click", function() {
@@ -45,7 +53,8 @@ resetBtn.addEventListener("click", function() {
   num2.value = 0;
   //removing all elements that is in the displayBox.
   // https://www.javascripttutorial.net/javascript-dom/javascript-removechild/
-  while(displayBox.firstChild) {
-    displayBox.removeChild(displayBox.firstChild);
-  }
+  // while(displayBox.firstChild) {
+  //   displayBox.removeChild(displayBox.firstChild);
+  // }
+  displayResult.innerHTML = "";
 });
